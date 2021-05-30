569ec4de0d28f92277b8610fa2d389aa98db2a6a
GlebPishchalo <gleb4ikq@gmail.com>
2021-05-30 14:58:14 +0300

    first and second tasks

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,21 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        if (sadArray.size <= 2) {
+            return sadArray
+        }
+        val happyArray = arrayListOf(firstArray[0])
+        val firstArray = sadArray.reversedArray()
+        var left = 0
+
+
+        for (i in 1 until firstArray.lastIndex) {
+            if (firstArray[i] <= (firstArray[left] + firstArray[i + 1])) {
+                happyArray.add(firstArray[i])
+                left = i
+            }
+        }
+        happyArray.add(firstArray[firstArray.lastIndex])
+        return happyArray.toIntArray().reversedArray()
     }
 }
--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -5,6 +5,18 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+val schet = (bill.sum()-bill[k])/2
+        if(schet==b)
+        {
+            return "bon appetit"
+        }
+        else
+            return (b-schet/2).toString()
+
+
+
+
+
+
     }
 }

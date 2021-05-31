2b2e466fe21bb33ca06ee8b39f9c1aae18e2f6b7
GlebPishchalo <gleb4ikq@gmail.com>
2021-05-31 16:12:42 +0300

    fix mistakes

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -4,12 +4,12 @@ class BillCounter {
 
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
-    fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
+    fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {{
         if((bill.sum()-bill[k])/2==b)
         {
             return "bon appetit"
         }
         else
             return (b-(bill.sum()-bill[k])/2).toString()
-    }
+    }}
 }
